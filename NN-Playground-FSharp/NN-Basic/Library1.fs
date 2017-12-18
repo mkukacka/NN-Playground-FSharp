018902fb376b492635e1772f0f68b98bc2f62996
Marek <mkukacka@gmail.com>
2017-12-18 20:27:24 +0100

    playing around with F# - first attempts at some code

---
+++ NN-Playground-FSharp/NN-Basic/Library1.fs
@@ -0,0 +1,17 @@
+﻿
+
+namespace NN_Basic
+
+type Class1() = 
+    member this.X = "F#"
+
+module ActivationFunctions =
+
+    let logSigmoidFunction potential lambda =
+        1.0 / (1.0 + System.Math.Exp(lambda * potential))
+
+
+module HelperFunctions =
+
+    let computePotential (weights:double list) (input:double list) : double = 
+        List.fold2 (fun acc x y -> acc + x * y) 0.0 weights input 
